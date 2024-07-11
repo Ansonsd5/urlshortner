7f780e8fc69e12aaa260c9246488e207d59c5597
Ansonsd5 <anson11sd@gmail.com>
2024-07-11 21:04:31 +0530

    feat: url shortner

--- url-shortner/vite.config.js
+++ url-shortner/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -8,7 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      'react-router-dom': require.resolve('react-router-dom')
     },
   },
-})
+});
