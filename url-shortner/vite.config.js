58a2009b9c5cb7f76441ceb7baf6e55a50c6ca10
Ansonsd5 <anson11sd@gmail.com>
2024-07-11 20:54:03 +0530

    feat: url shortner

--- url-shortner/vite.config.js
+++ url-shortner/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      'react-router-dom': require.resolve('react-router-dom')
     },
   },
 })
